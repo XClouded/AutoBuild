1aec903157a99fbec42cc1324c956ffc044f6179
leleliu008 <792793182@qq.com>
2014-10-20 15:03:40 +0800

    删除无用的资源

--- env.sh
+++
@@ -1,4 +0,0 @@
-basedir=`pwd`
-libdir=$basedir/libs/javamail-1.4.7
-export CLASSPATH=$libdir/dsn.jar:$libdir/gimap.jar:$libdir/imap.jar:$libdir/mailapi.jar:$libdir/pop3.jar:$libdir/smtp.jar:$CLASSPATH
-echo $CLASSPATH
